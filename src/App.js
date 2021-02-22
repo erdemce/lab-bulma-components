e937b944bfb8dd8096096aa0d0ece9191a12e441
erdemce <erdemce2234@gmail.com>
2021-02-23 00:59:41 +0100

    done except iteration 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import './App.css'
+import Navbar from "./navbar/Navbar"
+import FormField from "./formfield/FormField"
+import CoolButton from "./coolbutton/CoolButton"
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <React.Fragment>
+    <h1><Navbar/></h1>
+  <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+<FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+<FormField label="Password" type="password"/>
+
+<CoolButton isSmall isRounded isInfo isLight>Submit</CoolButton>
+
+</React.Fragment>)
 };
 
 export default App;
---
+++ src/coolbutton/CoolButton.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+let data={
+    isActive: 'is-active',
+    isBlack: 'is-black',
+    isCentered: 'is-centered',
+    isDanger: 'is-danger',
+    isDark: 'is-dark',
+    isFocused: 'is-focused',
+    isGrouped: 'is-grouped',
+    isHovered: 'is-hovered',
+    isInfo: 'is-info',
+    isInverted: 'is-inverted',
+    isLarge: 'is-large',
+    isLight: 'is-light',
+    isLink: 'is-link',
+    isLoading: 'is-loading',
+    isMedium: 'is-medium',
+    isOutlined: 'is-outlined',
+    isPrimary: 'is-primary',
+    isRight: 'is-right',
+    isRounded: 'is-rounded',
+    isSelected: 'is-selected',
+    isSmall: 'is-small',
+    isStatic: 'is-static',
+    isSuccess: 'is-success',
+    isText: 'is-text',
+    isWarning: 'is-warning',
+    isWhite: 'is-white',
+  }
+
+function CoolButton(props) {
+
+    let classValue=props.className+" button"
+    Object.keys(data).forEach(item=>{
+    
+        if(props[item]){
+            classValue=classValue+" "+data[item]
+        }
+    })
+    
+ 
+    return (
+
+       <button className={classValue}>{props.children}</button>
+        )  
+}
+
+export default CoolButton;
